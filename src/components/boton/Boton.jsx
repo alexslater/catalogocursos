997db7fef6a46db3217cea18e29c8d902586b248
alexslater <awslater@gmail.com>
2024-07-22 18:29:22 -0400

    [ReactJS] Boton

---
+++ src/components/boton/Boton.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import $ from 'jquery'; 
+import Popper from 'popper.js'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
+const Boton = (props) => {
+
+    const {texto, fn, estiloboton} = props
+    const estilo = "btn " + '"' + {estiloboton} + '"'
+
+    return (
+        <button type="button" className={estilo} onClick={() => fn()}>{props.texto}</button>
+    )
+}
+
+export default Boton
